05262e918c657213ce98046cfd5a158935a243b7
0914 0512 <774581304@qq.com>
2023-12-11 16:49:14 +0800

    feat:基础配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="box">
     <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
-
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 
 //引用ElementPlus插件与样式
-import ElementPlus from 'element-plus';
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 //配置element-plus 国际化
@@ -12,9 +12,8 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 const app = createApp(App)
 //安装ElementPlus插件
 app.use(ElementPlus, {
-    locale: zhCn//配置element-plus 国际化
+  locale: zhCn, //配置element-plus 国际化
 })
-
 
 //将应用挂载在#app节点上
 app.mount('#app')
