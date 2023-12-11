27639c52f6ebff714a326e7d25a491303f6e331e
0914 0512 <774581304@qq.com>
2023-12-11 15:44:42 +0800

    初始

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <h1>我是APP根组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
