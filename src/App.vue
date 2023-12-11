c9ddd3bfbec38b3bbe0942ecbedfda76f4c3cd8b
0914 0512 <774581304@qq.com>
2023-12-11 16:39:12 +0800

    feat:基础配置

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="box">
-    <h1>我是APP根组件</h1>
+    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+
   </div>
 </template>
 
 <script setup lang="ts">
+//引入图标组件
+import { Plus } from '@element-plus/icons-vue'
 </script>
 
-<style scoped lang="scss">
-.box {
-  width: 600px;
-  height: 400px;
-  background-color: black;
-  h1 {
-    color: white;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+//引用ElementPlus插件与样式
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+//配置element-plus 国际化
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+//获取应用对象实例
+const app = createApp(App)
+//安装ElementPlus插件
+app.use(ElementPlus, {
+    locale: zhCn//配置element-plus 国际化
+})
+
+
+//将应用挂载在#app节点上
+app.mount('#app')
